996ee34df27f8343aed8e62cc95c88a9e57923de
BANZOM <aditya.panwar.2208@gmail.com>
2023-11-05 18:03:47 +0530

    more for loops

--- 12_forLoop.sh
+++ 12_forLoop.sh
@@ -4,3 +4,17 @@ for i in 1 2 3 4 5
 do
     echo $i
 done
+
+
+# wild card
+for i in {6..10}
+do
+    echo $i
+done
+
+echo "_________________________"
+echo "List all files in current directory"
+for name in $(ls)
+do
+    echo $name
+done
